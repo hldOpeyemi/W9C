034c14145bd8ad3426fdbca0efa6ad654ea44561
Opeyemi <yemi.otu@outlook.com>
2023-09-21 21:20:41 -0600

    JS with Push and Pop

---
+++ app.js
@@ -0,0 +1,43 @@
+// Arrays
+let item_names = [`Premium`, `Luxury`, `ExtraLux`]
+    console.log(item_names);
+
+let item_prices = [`$2,000`, `$5,000`, `$10,000`]
+    console.log(item_prices);
+
+let in_stock = [`In stock`, `Out of Stock`, `Recalled`]
+    console.log(in_stock);
+
+
+// Variables
+let first_item_name = item_names[0]
+    console.log(first_item_name);
+
+let first_item_price = item_prices[0]
+    console.log(first_item_price);
+
+item_names.push(`ExtraExtraLux`)
+    console.log(item_names);
+
+item_prices.push(`$15,000`)
+    console.log(item_prices);
+
+in_stock.push(`Out of Production`)
+    console.log(in_stock);
+
+
+let last_item_name = item_names.pop()
+    console.log(last_item_name);
+
+let last_item_price = item_prices.pop()
+    console.log(last_item_price);
+
+let last_item_stock = in_stock.pop()
+    console.log(last_item_stock);
+
+
+
+
+
+
+
